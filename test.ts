b719726186f2ad3e25da6ff18125ed2e95abc320
CarlWilliamsBristol <125634779+CarlWilliamsBristol@users.noreply.github.com>
2023-02-20 18:16:32 +0000

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,9 @@
 // tests go here; this will not be compiled when this package is used as an extension.
+namespace tm1650DisplayTests {
+    export function configAndTurnOn() {
+        tm1650Display.configure("display1", DigitalPin.P1, DigitalPin.P0)
+        tm1650Display.displayOn("display1", 5)
+        tm1650Display.showString("HEL0")
+    }
+}
+tm1650display.ts
